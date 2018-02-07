524f8232e6594d52eced1a4281057f251e9197a3
haenvi <haenvi@hanmail.net>
2018-02-07 17:18:49 +0900

    start_study

---
+++ NettyStudy/src/main/haenvi/study/netty/server/NettyServer.java
@@ -0,0 +1,56 @@
+package haenvi.study.netty.server;
+
+
+import org.apache.log4j.BasicConfigurator;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class NettyServer {
+	
+	private static final int _PORT = 9090;
+	
+	public static void main(String args[]) {
+
+		BasicConfigurator.configure();
+		
+		EventLoopGroup parentGroup = new NioEventLoopGroup(1);
+		EventLoopGroup childGroup = new NioEventLoopGroup();
+		
+		try {
+			ServerBootstrap sb = new ServerBootstrap();
+			
+			sb.group(parentGroup, childGroup)
+			.channel(NioServerSocketChannel.class)
+			.option(ChannelOption.SO_BACKLOG, 100)
+			.handler(new LoggingHandler(LogLevel.INFO))
+			.childHandler(new ChannelInitializer<SocketChannel>() {
+				@Override
+				protected void initChannel(SocketChannel sc) throws Exception {
+					ChannelPipeline cp = sc.pipeline();
+					//TODO:
+					cp.addLast(null);
+				}
+			});
+
+			ChannelFuture cf = sb.bind(_PORT).sync();
+			cf.channel().closeFuture().sync();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			
+		} finally{
+			parentGroup.shutdownGracefully();
+			childGroup.shutdownGracefully();
+		}
+	}
+}
